0ea9071295876ca99e40fb71d2c72a21d53a798f
robbert <robbert@vdzon.com>
2019-07-26 10:12:49 +0200

    web interface for module

--- src/main/resources/html/js/app.js
+++ src/main/resources/html/js/app.js
@@ -5,6 +5,14 @@
     var $slider = $('.slider');
     $slider.aminoSlider();
 
+    function normalize(pos){
+        var min = 1200;
+        var max = 1800;
+        var diff = max-min
+        var posNr = Number(pos);
+        return min + ((posNr*diff)/100);
+    }
+
     $( ".slider" ).change(function() {
         $( "#output" ).html("changed");
         var arm1 = $( "#arm1" ).get("0")["dataset"]["value"];
@@ -12,7 +20,14 @@
         var arm3 = $( "#arm3" ).get("0")["dataset"]["value"];
         var arm4 = $( "#arm4" ).get("0")["dataset"]["value"];
         var arm5 = $( "#arm5" ).get("0")["dataset"]["value"];
-        $( "#output" ).html("changed:"+arm1+"-"+arm2+"-"+arm3+"-"+arm4);
+
+        arm1 = normalize(arm1);
+        arm2 = normalize(arm2);
+        arm3 = normalize(arm3);
+        arm4 = normalize(arm4);
+        arm5 = normalize(arm5);
+
+        $( "#output" ).html(arm1+"-"+arm2+"-"+arm3+"-"+arm4+"-"+arm5);
 
         $.ajax({
             type: "POST",
