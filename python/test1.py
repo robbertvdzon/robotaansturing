277d45c319e2bdb0a5bd6b272ba33300d4eb248a
robbert <robbert@vdzon.com>
2019-07-31 19:17:11 +0200

    schuivers

--- python/test1.py
+++ python/test1.py
@@ -1,6 +1,23 @@
+# 4 laag :  5.296 6.349 5.941 3.384
+# 4 hoog :  5.288 6.765 6.291 3.009
+
+# 3 laag : 6.429 5.096 5.445 3.239
+# 3 hoog : 7.088 5.104 5.936 2.273
+
+# 2 laag : 7.304 4.386 5.169 3.332
+# 2 hoog : 7.792 4.547 5.460 2.677
+
+# 1 laag : 7.810 4.350 2.846 5.104
+# 1 hoog : 7.846 5.120 2.846 4.375
+
+
+import time
 from smbus import SMBus
 from PCA9685 import PWM
-import time
+# Import the ADS1x15 module.
+import Adafruit_ADS1x15
+
+
 
 fPWM = 50
 i2c_address = 0x40 # (standard) adapt to your module
@@ -14,47 +31,29 @@ def setup():
     pwm = PWM(bus, i2c_address)
     pwm.setFreq(fPWM)
 
-def setDirection(direction, ch):
-    duty = a / 180 * direction + b
+def setDirection(duty, ch):
     pwm.setDuty(ch, duty)
-    #print "direction =", direction, "-> duty =", duty
-    time.sleep(0.003) # allow to settle
+
+def moveTo(duty0, duty1, duty2, duty3 ):
+    setDirection(duty0,0)
+    setDirection(duty1,1)
+    setDirection(duty2,2)
+    setDirection(duty3,3)
 
 print "starting"
 setup()
 
+# Main loop.
+moveTo (5.296 ,6.349 ,5.941, 3.384) # 4 hoog
 
-# read adc
-address = 0x48
-A0 = 0x40
-A1 = 0x41
-A2 = 0x42
-A3 = 0x43
-bus2 = SMBus(1)
-while True:
-    print ("--")
-    bus2.write_byte(address,A0)
-    value0 = bus2.read_byte(address)
-    print("bus0:%1.3f  " %(value0))
+# 4 laag :  5.296 6.349 5.941 3.384
+# 4 hoog :  5.288 6.765 6.291 3.009
 
-    bus2.write_byte(address,A1)
-    value1 = bus2.read_byte(address)
-    print("bus1:%1.3f  " %(value1))
+# 3 laag : 6.429 5.096 5.445 3.239
+# 3 hoog : 7.088 5.104 5.936 2.273
 
-    bus2.write_byte(address,A2)
-    value2 = bus2.read_byte(address)
-    print("bus2:%1.3f  " %(value2))
+# 2 laag : 7.304 4.386 5.169 3.332
+# 2 hoog : 7.792 4.547 5.460 2.677
 
-    bus2.write_byte(address,A3)
-    value3 = bus2.read_byte(address)
-    print("bus3:%1.3f  " %(value3))
-
-
-    setDirection(value0,0)
-    setDirection(value1,1)
-    setDirection(value2,2)
-    setDirection(value3,3)
-    time.sleep(0.1)
-
-
-print "done"
+# 1 laag : 7.810 4.350 2.846 5.104
+# 1 hoog : 7.846 5.120 2.846 4.375
