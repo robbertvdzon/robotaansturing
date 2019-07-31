c5cc667c20cdeb7d99bdaf17c9dedf0a2ea1f267
robbert <robbert@vdzon.com>
2019-07-31 19:45:22 +0200

    schuivers

--- python/test1.py
+++ python/test1.py
@@ -1,23 +1,6 @@
-# 4 laag :  5.296 6.349 5.941 3.384
-# 4 hoog :  5.288 6.765 6.291 3.009
-
-# 3 laag : 6.429 5.096 5.445 3.239
-# 3 hoog : 7.088 5.104 5.936 2.273
-
-# 2 laag : 7.304 4.386 5.169 3.332
-# 2 hoog : 7.792 4.547 5.460 2.677
-
-# 1 laag : 7.810 4.350 2.846 5.104
-# 1 hoog : 7.846 5.120 2.846 4.375
-
-
 import time
 from smbus import SMBus
 from PCA9685 import PWM
-# Import the ADS1x15 module.
-import Adafruit_ADS1x15
-
-
 
 fPWM = 50
 i2c_address = 0x40 # (standard) adapt to your module
@@ -25,10 +8,10 @@ channel = 0 # adapt to your wiring
 a = 8.5 # adapt to your servo
 b = 2  # adapt to your servo
 
-current0 =5.288
-current1 =6.765
-current2 =6.291
-current3 =3.009
+current0 =6.393
+current1 =5.908
+current2 =5.985
+current3 =2.514
 
 def setup():
     global pwm
@@ -63,10 +46,6 @@ def moveTo(duty0, duty1, duty2, duty3 ):
         setDirection(current2,2)
         setDirection(current3,3)
     time.sleep( 1 )
-
-
-
-
 
 print "starting"
 setup()
