c4ef6a4ba8be5284284ff78e292a0d5827e49e98
robbert <robbert@vdzon.com>
2020-01-03 11:36:06 +0100

    update

--- python/i2c_write.py
+++ python/i2c_write.py
@@ -9,6 +9,6 @@ address_2 = 0x05
 print(sys.argv[1])
 data_list = list(sys.argv[1])
 for i in data_list:
-    bus.write_byte(address_2, i)
+    bus.write_byte(address_2, int(ord(i)))
     time.sleep(.01)
 
