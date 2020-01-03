ec387c3e7d3f0a7d2a86c0f058919eaa6946cf1d
robbert <robbert@vdzon.com>
2020-01-03 11:38:02 +0100

    update

--- python/i2c_write.py
+++ python/i2c_write.py
@@ -4,9 +4,14 @@ import sys
 
 bus = smbus.SMBus(1)
 
-address_2 = 0x05
 
-print(sys.argv[1])
+
+address_2 = sys.argv[0]
+data = sys.argv[2]
+
+print(address_2)
+print(data)
+
 data_list = list(sys.argv[1])
 for i in data_list:
     bus.write_byte(address_2, int(ord(i)))
