72f0e74b239f3719613786f7035da157b9af790c
robbert <robbert@vdzon.com>
2020-01-19 11:10:57 +0100

    update

---
+++ src/main/java/com/vdzon/ui/Main.java
@@ -0,0 +1,18 @@
+package com.vdzon.ui;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    SwingUtilities.invokeLater(new Runnable() {
+      public void run() {
+        MyPanel mainPanel = new MyPanel();
+      }
+
+    });
+  }
+
+
+}
---
+++ src/main/java/com/vdzon/ui/MyPanel.java
@@ -0,0 +1,60 @@
+package com.vdzon.ui;
+
+import com.vdzon.berekenarmen.DoubleLine;
+import com.vdzon.berekenarmen.Segment;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+public class MyPanel extends JPanel {
+
+  private static final int PREF_W = 600;
+  private static final int PREF_H = PREF_W;
+
+  public MyPanel() {
+    JFrame f = new JFrame("Schaakrobot");
+    f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    f.getContentPane().add(this);
+    f.pack();
+    f.setLocationByPlatform(true);
+
+    final JTextField tf=new JTextField();
+    tf.setBounds(50,50, 150,20);
+    JButton b=new JButton("Click Here");
+    b.setBounds(50,100,95,30);
+    b.addActionListener(e -> button1(tf));
+    f.add(b);
+    f.add(tf);
+    f.setSize(400,400);
+    f.setLayout(null);
+
+    f.setExtendedState(JFrame.MAXIMIZED_BOTH);
+//    f.setUndecorated(true);
+    f.setVisible(true);
+
+
+
+  }
+
+  private void button1(JTextField tf) {
+    String[] cmd = {"sh","/home/pi/update.sh"};
+    try {
+      Runtime.getRuntime().exec(cmd);
+      System.exit(0);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+}
