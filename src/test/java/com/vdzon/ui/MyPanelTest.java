81a8d05618bd85319b751407ab22904bbe821373
robbert <robbert@vdzon.com>
2020-03-01 11:59:55 +0100

    langzamer

--- src/test/java/com/vdzon/ui/MyPanelTest.java
+++ src/test/java/com/vdzon/ui/MyPanelTest.java
@@ -7,7 +7,7 @@ public class MyPanelTest {
   @Test
   public void testSpeed(){
     MyPanel panel = new MyPanel();
-    panel.calcDelays(0,200,50);
+//    panel.calcDelays(0,200,50);
 //    System.out.println(panel.delayArm1 + ","+panel.delayArm2 + ","+panel.delayArm3);
 //    panel.calcDelays(100,200,50);
 //    System.out.println(panel.delayArm1 + ","+panel.delayArm2 + ","+panel.delayArm3);
