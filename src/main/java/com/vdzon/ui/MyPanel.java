1aca5199ca058459228ebb6b88892b9300a93df3
robbert <robbert@vdzon.com>
2020-03-01 11:11:59 +0100

    langzamer

--- src/main/java/com/vdzon/ui/MyPanel.java
+++ src/main/java/com/vdzon/ui/MyPanel.java
@@ -393,7 +393,7 @@ public class MyPanel extends JPanel {
 
   private void home(I2CDevice arm) {
     try {
-      if (arm != null) { arm.write("^H0000000000300000".getBytes()); }
+      if (arm != null) { arm.write("^H0000000000600000".getBytes()); }
       if (arm == arm1) { lastPos1 = 0; }
       if (arm == arm2) { lastPos2 = 0; }
       if (arm == arm3) { lastPos3 = 0; }
