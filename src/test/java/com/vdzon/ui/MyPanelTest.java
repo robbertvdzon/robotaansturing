bb30ebc2878069e6efc9fcdd605648e10bbbf702
robbert <robbert@vdzon.com>
2020-03-01 11:31:37 +0100

    langzamer

--- src/test/java/com/vdzon/ui/MyPanelTest.java
+++ src/test/java/com/vdzon/ui/MyPanelTest.java
@@ -8,17 +8,17 @@ public class MyPanelTest {
   public void testSpeed(){
     MyPanel panel = new MyPanel();
     panel.calcDelays(0,200,50);
-    System.out.println(panel.delayArm1 + ","+panel.delayArm2 + ","+panel.delayArm3);
-    panel.calcDelays(100,200,50);
-    System.out.println(panel.delayArm1 + ","+panel.delayArm2 + ","+panel.delayArm3);
-    panel.calcDelays(102,202,52);
-    System.out.println(panel.delayArm1 + ","+panel.delayArm2 + ","+panel.delayArm3);
-    panel.calcDelays(100,-200,50);
-    System.out.println(panel.delayArm1 + ","+panel.delayArm2 + ","+panel.delayArm3);
-    panel.calcDelays(100,200,50);
-    System.out.println(panel.delayArm1 + ","+panel.delayArm2 + ","+panel.delayArm3);
-    panel.calcDelays(100,200,50);
-    System.out.println(panel.delayArm1 + ","+panel.delayArm2 + ","+panel.delayArm3);
+//    System.out.println(panel.delayArm1 + ","+panel.delayArm2 + ","+panel.delayArm3);
+//    panel.calcDelays(100,200,50);
+//    System.out.println(panel.delayArm1 + ","+panel.delayArm2 + ","+panel.delayArm3);
+//    panel.calcDelays(102,202,52);
+//    System.out.println(panel.delayArm1 + ","+panel.delayArm2 + ","+panel.delayArm3);
+//    panel.calcDelays(100,-200,50);
+//    System.out.println(panel.delayArm1 + ","+panel.delayArm2 + ","+panel.delayArm3);
+//    panel.calcDelays(100,200,50);
+//    System.out.println(panel.delayArm1 + ","+panel.delayArm2 + ","+panel.delayArm3);
+//    panel.calcDelays(100,200,50);
+//    System.out.println(panel.delayArm1 + ","+panel.delayArm2 + ","+panel.delayArm3);
   }
 
 
